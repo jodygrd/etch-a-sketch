b30b247cd4ec17d714f86ea6879f996547a0bae0
jody lipkin <lipkinjody@gmail.com>
2022-11-26 15:04:37 -0800

    Create hover color-change

--- javascript.js
+++ javascript.js
@@ -3,5 +3,27 @@
     //make rows   
     //make columns
 
+// const para = document.createElement("p");
+// para.innerHTML = "This is a paragraph.";
+// document.getElementById("myDIV").appendChild(para);
+
+//CREATE GRID
+const grid_width = 16
+
+for (let i = 0; i < grid_width; i++) {
+    for (let i = 0; i < grid_width; i++) {
+        const cell = document.createElement("div")
+        cell.classList.add("cell");
+        document.getElementById("container").appendChild(cell)
+    }
+}
+
+//MAKE CELLS CHANGE ON MOUSE OVER
+const cells = document.querySelectorAll(".cell")
+cells.forEach(cell => {cell.addEventListener('mouseover', function(){
+    cell.style.backgroundColor = "purple";
+    }
+)});
+
 
 
