069730e4141fc9677c8fab84c1863c00c3730d11
jody lipkin <lipkinjody@gmail.com>
2022-11-26 18:12:11 -0800

    Add button and interactivity for grid size

--- javascript.js
+++ javascript.js
@@ -7,25 +7,54 @@
 // para.innerHTML = "This is a paragraph.";
 // document.getElementById("myDIV").appendChild(para);
 
-//CREATE GRID
-const grid_width = 50
-const cell_width = 400 / grid_width
+// //GET GRID WIDTH
+// let grid_width = 16
+// createGrid(grid_width);
 
-for (let i = 0; i < grid_width; i++) {
+createGrid(16);
+sketch();
+getSize();
+
+//QUERY FOR SIZE;
+function getSize(){
+    const buttons = document.querySelectorAll("button")
+    buttons.forEach( button => button.addEventListener("click", function(){
+        document.getElementById("container").innerHTML = ""
+        let grid_width = 0;
+        do {
+            grid_width = prompt("Pick a number between 10 and 100!");
+        } while (grid_width < 10 || grid_width > 100)
+        createGrid(grid_width);
+        sketch();   
+    }))
+}
+
+
+
+//CREATE GRID
+function createGrid(grid_width){
+    const cell_width = 400 / grid_width
     for (let i = 0; i < grid_width; i++) {
-        const cell = document.createElement("div")
-        cell.classList.add("cell");
-        document.getElementById("container").appendChild(cell)
-        cell.style.width = cell_width + "px"
-    }
+        for (let i = 0; i < grid_width; i++) {
+            const cell = document.createElement("div")
+            cell.classList.add("cell");
+            document.getElementById("container").appendChild(cell)
+            cell.style.width = cell_width + "px"
+        }
+    }   
+
 }
 
 //MAKE CELLS CHANGE ON MOUSE OVER
-const cells = document.querySelectorAll(".cell")
-cells.forEach(cell => {cell.addEventListener('mouseover', function(){
-    cell.style.backgroundColor = "purple";
+function sketch() {
+    const cells = document.querySelectorAll(".cell")
+    cells.forEach(cell => {cell.addEventListener('mouseover', function(){
+        cell.style.backgroundColor = "purple";
     }
 )});
+}
+
+
 
 
 
